9a587da1fa894674277d04d3d8a3506edbca8e00
RakshitaTantry28 <55851040+RakshitaTantry28@users.noreply.github.com>
2024-09-12 20:11:59 +0000

    Added ci.yaml pylint

--- src/hw2/hw2_debugging.py
+++ src/hw2/hw2_debugging.py
@@ -5,6 +5,7 @@ merge sort to produce a sorted version of the same
 import sys
 import os
 # Add the src/hw2 directory to the system path
+# pylint: disable=wrong-import-order
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src/hw2')))
 from rand import random_array
 
